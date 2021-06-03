a75a6e3a8e22825e68cffae7a0d3168b8ef78086
Jeandre Rohlandt <jrohlandt@gmail.com>
2021-06-03 09:43:15 +0200

    simple script to create video from image files and audio file.

--- video-from-images.bat
+++ video-from-images.bat
@@ -1,5 +1,6 @@
 rem This is a comment
 
-ffmpeg\ffmpeg.exe -h
+ffmpeg\ffmpeg.exe -framerate 1 -start_number 4551 -i images/image_%%4d.jpg ^
+-i audio/audiofile01.mp3 -shortest -c:v mpeg4 -q:v 2 out/out.mp4
 
 pause
