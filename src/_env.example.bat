0fd52152881c383e6f144cd44a23f11103b14ab3
Jeandre Rohlandt <jrohlandt@gmail.com>
2021-06-08 08:54:00 +0200

    ffprobe

--- src/_env.example.bat
+++ src/_env.example.bat
@@ -1,4 +1,5 @@
 set FFMPEGPATH=C:\Users\you\code\ffmpeg\ffmpeg.exe
+set FFPROBEPATH=C:\Users\you\code\ffmpeg\ffprobe.exe
 set OUTDIR=C:\Users\you\out
 set PATHV=C:\Users\you\videos
 set PATHA=C:\Users\you\audio
---
+++ src/get-file-details.bat
@@ -0,0 +1,5 @@
+call _init.bat
+
+@REM %FFPROBEPATH% -print_format json %PATHV%/ukelele.mp4
+
+%FFPROBEPATH% -print_format json %PATHV%/videofile04.avi
